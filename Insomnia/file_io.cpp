430a1e906dee6f918235d128a777957ee0a36ccd
KrossX <krossx@yahoo.com>
2015-11-10 13:40:13 -0300

    lowercase part2
    I'm sure there's a better way though.

--- Insomnia/file_io.cpp
+++ Insomnia/file_io.cpp
@@ -15,8 +15,8 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "Insomnia.h"
-#include "FileIO.h"
+#include "insomnia.h"
+#include "file_io.h"
 #include <fstream>
 
 //#include <stdlib.h>
